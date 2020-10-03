71e38938e9610bd9f1967467c1e4a0d96e56c9f4
Romain Bricout <thepizzadu62@gmail.com>
2020-10-04 00:30:52 +0200

    Introduction message

---
+++ main.py
@@ -0,0 +1,15 @@
+print("""
+Welcome in Guess the Number! This is a game where the computer chooses a random number and you must guess it!
+
+There are 5 levels of difficulty:
+1. Very easy: the number is between 1 and 10
+2. Easy: the number is between 1 and 20
+3. Medium: the number is between 1 and 50
+4. Hard: the number is between 1 and 100
+5. Very hard: the number is between 1 and 250
+
+At each of your tries to guess the number, the computer will give you a clue to guess it.
+You lose if you don't guess the number before the computer has given you the 10th clue.
+
+Please choose a level.
+""")
